cdcf30f00a27bc5fd3dec8ed7a41c9c3809b35bb
안서연 <asaya0818@yonsei.ac.kr>
2023-05-19 17:22:25 +0900

    update

--- musical_project/musical_project/settings.py
+++ musical_project/musical_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-$co$loz@9*jv#g9+^8$wp7elb8z)v8xx-8=+nmm&q@-ch5$)o_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ybigtada22.pythonanywhere.com']
 
 
 # Application definition
